8ecb9c3029202c5d1c185414701468b0f67cadfc
pkrasz <pl.kraszewski@gmial.com>
2022-09-15 18:34:12 +0200

    add CharactersView

---
+++ DisneyWorld/Features/Characters/CharactersView.swift
@@ -0,0 +1,44 @@
+//
+//  CharactersView.swift
+//  DisneyWorld
+//
+//  Created by Paweł Kraszewski on 15/09/2022.
+//
+
+import UIKit
+
+class CharactersView: UIView {
+
+    //MARK: - Subview
+    
+    
+    
+    //MARK: - Initializator
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+        setupSubviews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Setup
+    
+    func setupView() {
+        
+    }
+    
+    func setupSubviews() {
+        
+    }
+    
+    func setupConstraints() {
+        
+    }
+    
+}
