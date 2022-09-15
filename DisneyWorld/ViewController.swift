815ba3f904760706088931204bab23024baaf852
pkrasz <pl.kraszewski@gmial.com>
2022-09-15 16:11:19 +0200

    create new Xcode Project

---
+++ DisneyWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DisneyWorld
+//
+//  Created by Paweł Kraszewski on 15/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
